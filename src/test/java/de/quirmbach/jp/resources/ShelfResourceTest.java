b208acd2f540c21a6164f9fa3a2fbcc00049e296
Jan-Philip Quirmbach <jp.quirmbach@gmail.com>
2022-07-02 13:15:45 +0200

    Add test

--- src/test/java/de/quirmbach/jp/resources/ShelfResourceTest.java
+++ src/test/java/de/quirmbach/jp/resources/ShelfResourceTest.java
@@ -1,7 +1,15 @@
 package de.quirmbach.jp.resources;
 
+import de.quirmbach.jp.shelf.client.OpenLibraryClient;
+import de.quirmbach.jp.shelf.dto.AuthorDto;
+import de.quirmbach.jp.shelf.dto.BookDto;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -9,13 +17,34 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class ShelfResourceTest {
 
+    @InjectMock
+    OpenLibraryClient openLibraryClient;
+
+    @BeforeEach
+    public void setup() {
+        Mockito.when(openLibraryClient.findBookByEditionKey("123"))
+                .thenReturn(
+                        new BookDto(
+                                List.of("JP"),
+                                List.of("123"),
+                                List.of("321"),
+                                List.of(123),
+                                "Test Book",
+                                "key",
+                                List.of(new AuthorDto("TestAuthor"))
+                        )
+                );
+    }
+
     @Test
-    public void testHelloEndpoint() {
+    public void testIsbnEndpoint() {
         given()
-          .when().get("/shelf")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .when().get("/shelf/isbn/123")
+                .then()
+                .statusCode(200)
+                .body("full_title", is("Test Book"))
+                .body("key", is("key"))
+                .body("authors[0].key", is("TestAuthor"));
     }
 
 }
